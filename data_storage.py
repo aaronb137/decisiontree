76e69cde175924f239d153bd073a3f6a748c3187
Aaron Bartee <zeebra901@nevada.unr.edu>
2023-09-14 15:27:04 -0700

    added data storage implementation and test scripts

---
+++ data_storage.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+# file_name = "cat_dog_data.csv"
+# data = np.genfromtxt(file_name, dtype=str, delimiter=',')
+
+# array builder
+def build_nparray(data):
+    feature_vectors, label_points = [], []
+    values = data[1:][0:]
+
+    for row in values:
+        label_points.append(row[-1])
+        feature_vectors.append(row[:-1])
+
+    feature_vectors = np.array(feature_vectors, dtype=np.float32)
+    label_points = np.array(label_points, dtype=np.int32)
+    
+    return feature_vectors, label_points
+
+# list builder
+def build_list(data):
+    feature_vectors, label_points = [], []
+    values = data[1:][0:]
+
+    for row in values:
+        label_points.append(row[-1])
+        feature_vectors.append(row[:-1])
+    
+    feature_vectors = np.array(feature_vectors, dtype=np.float32)
+    label_points = np.array(label_points, dtype=np.int32)
+
+    feature_vectors = feature_vectors.tolist()
+    label_points = label_points.tolist()
+
+    return feature_vectors, label_points
+
+# dictionary builder
+def build_dict(data):
+    variable_dict = {}
+    label_dict = {}
+
+    header_values = data[0, :-1]
+    feature_values = data[1:, :-1]
+    output_values = data[1:, -1]
+
+    feature_values = np.array(feature_values, dtype=np.float32)
+    output_values = np.array(output_values, dtype=np.int32)
+    feature_values = feature_values.tolist()
+    output_values = output_values.tolist()
+
+    for array in feature_values:
+        array_dict = {}
+        for value_index, value in enumerate(array):
+            array_dict[header_values[value_index]] = value
+        variable_dict[len(variable_dict)] = array_dict
+    
+    for i in range(len(output_values)):
+        label_dict[len(label_dict)] = output_values[i]
+
+    return variable_dict, label_dict
+
+# vector_values, label_values = build_nparray(data)
+# print(vector_values,label_values)
+
+# vectors, labels = build_list(data)
+# print(vectors, labels)
+
+# variable, label = build_dict(data)
+# print(variable)
