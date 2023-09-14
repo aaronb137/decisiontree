0ab52cf3a094f1899bca79663cd102deefe5aa20
aaronb137 <zeebra901@gmail.com>
2023-09-14 15:32:23 -0700

    adding rest of files from project

---
+++ decision_trees.py
@@ -0,0 +1,17 @@
+import numpy as np
+import math as m
+
+def DT_train_binary(X,Y,max_depth):
+    print("test")
+
+def DT_test_binary(X,Y,DT):
+    print("fart1")
+
+def DT_make_prediction(x,DT):
+    print("fart2")
+
+def DT_train_real(X,Y,max_depth):
+    print("fart3")
+
+def DT_test_real(X,Y,DT):
+    print("fart4")
